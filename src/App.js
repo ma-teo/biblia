f5161ab70dfae798c3bcfd73be62a2dffacca6c8
mateo <mateo.tarasiuk@gmail.com>
2020-06-16 20:24:33 +0200

    Change app component to function

--- src/App.js
+++ src/App.js
@@ -1,101 +1,52 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Loading from './components/Loading'
 import VerseTable from './components/VerseTable'
 import SearchBar from './components/SearchBar'
 import { BIBLES } from './variables'
 
-let VERSES
-let BIBLE
+export default () => {
+  const [bible, setBible] = useState(localStorage.getItem('bible') || 0)
+  const [book, setBook] = useState(localStorage.getItem('book') || 0)
+  const [chapter, setChapter] = useState(localStorage.getItem('chapter') || 0)
+  const [verses, setVerses] = useState()
 
-class App extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      bible: localStorage.getItem('bible') || 0,
-      book: localStorage.getItem('book') || 0,
-      chapter: localStorage.getItem('chapter') || 0,
-      loading: true
-    }
-
-    this.fetchBible(this.state.bible)
-
-    this.handleBibleChange = this.handleBibleChange.bind(this)
-    this.handleBookChange = this.handleBookChange.bind(this)
-    this.handleChapterChange = this.handleChapterChange.bind(this)
-  }
-
-  fetchBible(bible) {
+  useEffect(() => {
     fetch(`/bible/${BIBLES[bible]}.json`)
     .then(resp => resp.json())
-    .then(resp => {
-      BIBLE = resp
-      VERSES = BIBLE[this.state.book][this.state.chapter]
+    .then(resp => setVerses(resp))
+  }, [bible])
 
-      this.setState({
-        verses: VERSES,
-        loading: false
-      })
-    })
+  useEffect(() => localStorage.setItem('bible', bible), [bible])
+  useEffect(() => localStorage.setItem('book', book), [book])
+  useEffect(() => localStorage.setItem('chapter', chapter), [chapter])
+
+  useEffect(() => window.scrollTo(0,0))
+
+  function handleBibleChange(e) {
+    setVerses()
+    setBible(e.target.value)
   }
 
-  handleBibleChange(e) {
-    const bible = parseInt(e.target.value)
-
-    this.setState({
-      bible: bible,
-      loading: true
-    }, () => {
-      this.fetchBible(this.state.bible)
-      localStorage.setItem('bible', this.state.bible)
-    })
+  function handleBookChange(e) {
+    setBook(e.target.value)
+    setChapter(0)
   }
 
-  handleBookChange(e) {
-    const book = parseInt(e.target.value)
-    VERSES = BIBLE[book][0]
-
-    this.setState({
-      book: book,
-      chapter: 0,
-      verses: VERSES
-    }, () => {
-      localStorage.setItem('book', this.state.book)
-      localStorage.setItem('chapter', this.state.chapter)
-    })
+  function handleChapterChange(e) {
+    setChapter(e.target.value)
   }
 
-  handleChapterChange(e) {
-    const chapter = parseInt(e.target.value)
-    VERSES = BIBLE[this.state.book][chapter]
-
-    this.setState({
-      chapter: chapter,
-      verses: VERSES
-    }, () => {
-      localStorage.setItem('chapter', chapter)
-    })
-  }
-
-  render() {
-    window.scrollTo(0,0)
-
-    return (
-      <>
-        <SearchBar
-          bible={this.state.bible}
-          book={this.state.book}
-          chapter={this.state.chapter}
-          onBibleChange={this.handleBibleChange}
-          onBookChange={this.handleBookChange}
-          onChapterChange={this.handleChapterChange}
-        />
-        {this.state.loading ? <Loading /> :
-        <VerseTable
-          verses={this.state.verses}
-        />}
-      </>
-    )
-  }
+  return (
+    <>
+      <SearchBar
+        bible={bible}
+        book={book}
+        chapter={chapter}
+        onBibleChange={handleBibleChange}
+        onBookChange={handleBookChange}
+        onChapterChange={handleChapterChange}
+      />
+      {verses ? <VerseTable verses={verses[book][chapter]} /> : <Loading />}
+    </>
+  )
 }
-
-export default App
