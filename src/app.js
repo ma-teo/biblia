0ffc67cb8abbbb90d6e91c5ae388bb4e1eeedc6c
mateo <mateo.tarasiuk@gmail.com>
2020-08-16 13:09:33 +0200

    Update Variables State

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ import SearchBar from './components/search-bar'
 import VerseTable from './components/verse-table'
 
 const App = () => {
-  const [variables, setVariables] = useState()
+  const [variables, setVariables] = useState(JSON.parse(localStorage.getItem('variables')))
   const [bible, setBible] = useState(localStorage.getItem('bible') || 0)
   const [book, setBook] = useState(localStorage.getItem('book') || 0)
   const [chapter, setChapter] = useState(localStorage.getItem('chapter') || 0)
@@ -22,6 +22,7 @@ const App = () => {
     .then(data => setVerses(data))
   }, [bible, book, chapter])
 
+  useEffect(() => localStorage.setItem('variables', JSON.stringify(variables)), [variables])
   useEffect(() => localStorage.setItem('bible', bible), [bible])
   useEffect(() => localStorage.setItem('book', book), [book])
   useEffect(() => localStorage.setItem('chapter', chapter), [chapter])
