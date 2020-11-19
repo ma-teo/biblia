bde15d7c4ad14fbbc1511e6eb7e5a0800ecfaa89
mateo <mateo.tarasiuk@gmail.com>
2020-11-19 09:23:26 +0100

    Remove SW handler

--- src/sw.js
+++ src/sw.js
@@ -2,7 +2,3 @@ importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox
 
 workbox.core.skipWaiting()
 workbox.precaching.precacheAndRoute(self.__WB_MANIFEST)
-
-workbox.routing.registerRoute(new workbox.routing.NavigationRoute(workbox.precaching.createHandlerBoundToURL('index.html'), {
-  denylist: [/\/[^/?]+\.[^/]+$/],
-}))
