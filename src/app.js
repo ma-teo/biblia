4859324e64986ac215dd422c840a6cb4ba61020f
mateo <mateo.tarasiuk@gmail.com>
2021-01-18 09:32:24 +0100

    Add search params for selected verses

--- src/app.js
+++ src/app.js
@@ -1,13 +1,16 @@
 import { useState, useEffect } from 'react'
+import { useSearchParams } from 'react-router-dom'
 import Loading from './components/loading'
 import SearchBar from './components/search-bar'
 import VerseTable from './components/verse-table'
 
 const App = () => {
+  const [query, setQuery] = useSearchParams()
+
   const [variables, setVariables] = useState(JSON.parse(localStorage.getItem('variables')))
-  const [bible, setBible] = useState(localStorage.getItem('bible') || 0)
-  const [book, setBook] = useState(localStorage.getItem('book') || 0)
-  const [chapter, setChapter] = useState(localStorage.getItem('chapter') || 0)
+  const [bible, setBible] = useState(query.get('bible') || localStorage.getItem('bible') || 0)
+  const [book, setBook] = useState(query.get('book') || localStorage.getItem('book') || 0)
+  const [chapter, setChapter] = useState(query.get('chapter') || localStorage.getItem('chapter') || 0)
   const [verses, setVerses] = useState(JSON.parse(localStorage.getItem('verses')))
 
   useEffect(async () => {
@@ -25,6 +28,12 @@ const App = () => {
   useEffect(() => localStorage.setItem('book', book), [book])
   useEffect(() => localStorage.setItem('chapter', chapter), [chapter])
   useEffect(() => localStorage.setItem('verses', JSON.stringify(verses)), [verses])
+
+  useEffect(() => query.set('bible', bible), [bible])
+  useEffect(() => query.set('book', book), [book])
+  useEffect(() => query.set('chapter', chapter), [chapter])
+
+  useEffect(() => setQuery(query), [bible, book, chapter])
 
   useEffect(() => window.scrollTo(0,0))
 
--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import { StrictMode } from 'react'
 import { render } from 'react-dom'
+import { BrowserRouter } from 'react-router-dom'
 import App from './app'
 import { register } from './sw-register'
 import './index.css'
 
 render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </StrictMode>
 , document.getElementById('app'))
 
